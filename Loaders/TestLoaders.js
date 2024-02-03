942bb5ee49f0a8bb996f1dda8cd33d4dc00e4a97
Farshad Nabizadeh <farshadn@hotelistan.com>
2024-02-03 07:46:26 +0300

    server added

--- Loaders/TestLoaders.js
+++ Loaders/TestLoaders.js
@@ -4,8 +4,10 @@ function loadAndApplyTemplate(templateUrl, data, target) {
         url: templateUrl,
         cache: true,
         success: function(templateContent) {
+            console.log(templateContent); // Check the template content
             var template = Handlebars.compile(templateContent);
             var html = template(data);
+            console.log(html); // Check the generated HTML
             $(target).html(html);
         }
     });

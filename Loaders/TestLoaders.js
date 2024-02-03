b588ea1693bbedb534f9b29701e6370e66c10640
Farshad Nabizadeh <farshadn@hotelistan.com>
2024-02-03 06:39:00 +0300

    hbs added

---
+++ Loaders/TestLoaders.js
@@ -0,0 +1,22 @@
+// Function to load a template file, compile it with Handlebars, and apply the data
+function loadAndApplyTemplate(templateUrl, data, target) {
+    $.ajax({
+        url: templateUrl,
+        cache: true,
+        success: function(templateContent) {
+            var template = Handlebars.compile(templateContent);
+            var html = template(data);
+            $(target).html(html);
+        }
+    });
+}
+
+$(function() {
+    var data = [
+        { name: "Item 1", description: "Description of item 1" },
+        { name: "Item 2", description: "Description of item 2" },
+        { name: "Item 3", description: "Description of item 3" }
+    ];
+
+    loadAndApplyTemplate('itemTemplate.hbs', data, '.container');
+});
